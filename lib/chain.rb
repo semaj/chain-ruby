d538f94641ac8ce015c440e256653ea03d2eb9c8
Ryan Smith <r@32k.io>
2014-09-04 14:37:18 -0700

    First pass at client class

--- lib/chain.rb
+++ lib/chain.rb
@@ -4,14 +4,14 @@ require 'json'
 require 'thread'
 require 'uri'
 
-# A module that wraps the Chain HTTP API.
+# A module that wraps the Chain SDK.
 module Chain
   autoload :Sweeper, 'chain/sweeper'
-
-  @conn_mutex = Mutex.new
+  autoload :Client, 'chain/client'
+  autoload :Conn, 'chain/conn'
 
   GUEST_KEY = 'GUEST-TOKEN'
-  API_URL = URI('https://api.chain.com')
+  CHAIN_URL = 'https://api.chain.com'
 
   # A collection of root certificates used by api.chain.com
   CHAIN_PEM = File.expand_path('../../chain.pem', __FILE__)
@@ -24,249 +24,28 @@ module Chain
   # the HTTP request or the parsing of the response body.
   ChainError = Class.new(StandardError)
 
-  # Provide a Bitcoin address.
-  # Returns basic details for a Bitcoin address (hash).
-  def self.get_address(address)
-    get("/#{API_VERSION}/#{block_chain}/addresses/#{address}")
+  def self.default_client=(c)
+    @default_client = c
   end
 
-  # Provide an array of Bitcoin addresses.
-  # Returns an array of basic details for a set of Bitcoin address (hash).
-  def self.get_addresses(addresses)
-    self.get_address(addresses.join(','))
+  def self.default_client
+    @default_client ||= Client.new
   end
 
-  # Provide a Bitcoin address.
-  # Returns unspent transaction outputs for a Bitcoin address (array of hashes).
-  def self.get_address_unspents(address)
-    get("/#{API_VERSION}/#{block_chain}/addresses/#{address}/unspents")
+  def self.method_missing(sym, *args, &block)
+    default_client.send(sym, *args, &block)
   end
 
-  # Provide an array of Bitcoin addresses.
-  # Returns an array of unspent transaction outputs
-  # for a set of Bitcoin address (array of hashes).
-  def self.get_addresses_unspents(addresses)
-    self.get_address_unspents(addresses.join(','))
-  end
-
-  # Provide a Bitcoin address.
-  # Returns transactions for a Bitcoin address (array of hashes).
-  def self.get_address_transactions(address, options={})
-    get("/#{API_VERSION}/#{block_chain}/addresses/#{address}/transactions",
-      options)
-  end
-
-  # Provide an array of Bitcoin address.
-  # Returns an array of transactions for a set of Bitcoin address
-  # (array of hashes).
-  def self.get_addresses_transactions(addresses, options={})
-    self.get_address_transactions(addresses.join(','), options)
-  end
-
-  # Provide a Bitcoin address.
-  # Returns all op_return data associated with address.
-  def self.get_address_op_returns(address)
-    get("/#{API_VERSION}/#{block_chain}/addresses/#{address}/op-returns")
-  end
-
-  # Provide a Bitcoin transaction.
-  # Returns basic details for a Bitcoin transaction (hash).
-  def self.get_transaction(hash)
-    get("/#{API_VERSION}/#{block_chain}/transactions/#{hash}")
-  end
-
-  # Provide a Bitcoin transaction.
-  # Returns the OP_RETURN string (if it exists) for a Bitcoin transaction(hash).
-  def self.get_transaction_op_return(hash)
-    get("/#{API_VERSION}/#{block_chain}/transactions/#{hash}/op-return")
-  end
-
-  # Provide a hex encoded, signed transaction.
-  # Returns the newly created Bitcoin transaction hash (string).
-  def self.send_transaction(hex)
-    r = put("/#{API_VERSION}/#{block_chain}/transactions", {hex: hex})
-    r["transaction_hash"]
-  end
-
-  # Provide a Bitcoin block hash or height.
-  # Returns basic details for a Bitcoin block (hash).
-  def self.get_block(hash_or_height)
-    get("/#{API_VERSION}/#{block_chain}/blocks/#{hash_or_height}")
-  end
-
-  # Get latest Bitcoin block.
-  # Returns basic details for latest Bitcoin block (hash).
-  def self.get_latest_block
-    get("/#{API_VERSION}/#{block_chain}/blocks/latest")
-  end
-
-  # Provide a Bitcoin block id.
-  # Returns all op_return data contained in a block.
-  def self.get_block_op_returns(hash_or_height)
-    get("/#{API_VERSION}/#{block_chain}/blocks/#{hash_or_height}/op-returns")
-  end
-
-  def self.create_webhook(url, id=nil)
-    body = {}
-    body[:url] = url
-    body[:id] = id unless id.nil?
-    post("/#{API_VERSION}/webhooks", body)
-  end
-  singleton_class.send(:alias_method, :create_webhook_url, :create_webhook)
-
-  def self.list_webhooks
-    get("/#{API_VERSION}/webhooks")
-  end
-  singleton_class.send(:alias_method, :list_webhook_url, :list_webhooks)
-
-  def self.update_webhook(id, url)
-    put("/#{API_VERSION}/webhooks/#{id}", {url: url})
-  end
-  singleton_class.send(:alias_method, :update_webhook_url, :update_webhook)
-
-  def self.delete_webhook(id)
-    delete("/#{API_VERSION}/webhooks/#{id}")
-  end
-  singleton_class.send(:alias_method, :delete_webhook_url, :delete_webhook)
-
-  def self.create_webhook_event(id, opts={})
-    body = {}
-    body[:event] = opts[:event] || 'address-transaciton'
-    body[:block_chain] = opts[:block_chain] || self.block_chain
-    body[:address] = opts[:address] || raise(ChainError,
-      "Must specify address when creating a Webhook Event.")
-    unless opts[:confirmations].nil?
-      body[:confirmations] = opts[:confirmations]
-    end
-    post("/#{API_VERSION}/webhooks/#{id}/events", body)
-  end
-
-  def self.list_webhook_events(id)
-    get("/#{API_VERSION}/webhooks/#{id}/events")
-  end
-
-  def self.delete_webhook_event(id, event, address)
-    delete("/#{API_VERSION}/webhooks/#{id}/events/#{event}/#{address}")
-  end
-
-  # Set the key with the value found in your settings page on https://chain.com
-  # If no key is set, Chain's guest token will be used. The guest token
-  # should not be used for production services.
-  def self.api_key=(key)
-    $stderr.puts("Chain.com is deprecating api_key. Use api_key_id.")
-    @api_key = key
-  end
-
-  def self.api_key_id=(id)
-    @api_key_id = id
-  end
-
-  def self.api_key_secret=(secret)
-    @api_key_secret = secret
-  end
-
-  private
-
-  def self.post(path, body)
-    make_req!(Net::HTTP::Post, path, encode_body!(body))
-  end
-
-  def self.put(path, body)
-    make_req!(Net::HTTP::Put, path, encode_body!(body))
-  end
-
-  def self.get(path, params={})
-    path = path + "?" + URI.encode_www_form(params) unless params.empty?
-    make_req!(Net::HTTP::Get, path)
-  end
-
-  def self.delete(path)
-    make_req!(Net::HTTP::Delete, path)
-  end
-
-  def self.make_req!(type, path, body=nil)
-    conn do |c|
-      req = type.new(API_URL.request_uri + path)
-      req.basic_auth(api_key_id, api_key_secret)
-      req['Content-Type'] = 'application/json'
-      req['User-Agent'] = 'chain-ruby/0'
-      req.body = body
-      resp = c.request(req)
-      resp_code = Integer(resp.code)
-      resp_body = parse_resp(resp)
-      if resp_code / 100 != 2
-        raise(ChainError, "HTTP Request Error: #{resp_body['message']}")
-      end
-      return resp_body
-    end
-  end
-
-  def self.encode_body!(hash)
-    begin
-      JSON.dump(hash)
-    rescue => e
-      raise(ChainError, "JSON encoding error: #{e.message}")
-    end
-  end
-
-  def self.parse_resp(resp)
-      begin
-        JSON.parse(resp.body)
-      rescue => e
-        raise(ChainError, "JSON decoding error: #{e.message}")
-      end
-  end
-
-  def self.conn
-    @conn ||= establish_conn
-    @conn_mutex.synchronize do
-      begin
-        return yield(@conn)
-      rescue => e
-        @conn = nil
-        raise(ChainError, "Connection error: #{e.message}")
+  def self.url
+    @url ||= begin
+      URI(ENV['CHAIN_URL'] || CHAIN_URL).tap do |u|
+        u.user ||= GUEST_KEY
       end
     end
-  end
-
-  def self.establish_conn
-    Net::HTTP.new(API_URL.host, API_URL.port).tap do |c|
-      c.use_ssl = true
-      c.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      c.ca_file = CHAIN_PEM
-    end
-  end
-
-  def self.api_key_id
-    @api_key_id || api_key
-  end
-
-  def self.api_key_secret
-    @api_key_secret || secret_from_env || ''
-  end
-
-  def self.api_key
-    @api_key || key_from_env || GUEST_KEY
-  end
-
-  def self.key_from_env
-    if url = ENV['CHAIN_URL']
-      URI.parse(url).user
-    end
-  end
-
-  def self.secret_from_env
-    if url = ENV['CHAIN_URL']
-      URI.parse(url).password
-    end
-  end
-
-  def self.block_chain=(net)
-    @block_chain = net
   end
 
   def self.block_chain
-    @block_chain || BLOCK_CHAIN
+    BLOCK_CHAIN
   end
 
 end
---
+++ lib/chain/client.rb
@@ -0,0 +1,158 @@
+module Chain
+  class Client
+
+    attr_accessor :block_chain
+
+    # url, key_id, and key_secret can be configured by
+    # passing them in the options hash. Otherwise they will be read
+    # from the environment. Finally, if the environment does not contain
+    # these values they will be filled in the the guest token information.
+    # Guest tokens are limited in their access to the Chain API.
+    def initialize(opts={})
+      url = opts[:url] || Chain.url
+      key_id = opts[:key_id] || Chain.url.user
+      key_secret = opts[:key_secret] || Chain.url.password
+      @block_chain = opts[:block_chain] || Chain.block_chain
+      @conn = Conn.new(url, key_id, key_secret)
+    end
+
+    # Provide a Bitcoin address.
+    # Returns basic details for a Bitcoin address (hash).
+    def get_address(address)
+      @conn.get("/#{API_VERSION}/#{block_chain}/addresses/#{address}")
+    end
+
+    # Provide an array of Bitcoin addresses.
+    # Returns an array of basic details for a set of Bitcoin address (hash).
+    def get_addresses(addresses)
+      get_address(addresses.join(','))
+    end
+
+    # Provide a Bitcoin address.
+    # Returns unspent transaction outputs for a Bitcoin address
+    # (array of hashes).
+    def get_address_unspents(address)
+      @conn.get("/#{API_VERSION}/#{block_chain}/addresses/#{address}/unspents")
+    end
+
+    # Provide an array of Bitcoin addresses.
+    # Returns an array of unspent transaction outputs
+    # for a set of Bitcoin address (array of hashes).
+    def get_addresses_unspents(addresses)
+      get_address_unspents(addresses.join(','))
+    end
+
+    # Provide a Bitcoin address.
+    # Returns transactions for a Bitcoin address (array of hashes).
+    def get_address_transactions(address, options={})
+      url = "/#{API_VERSION}/#{block_chain}/addresses/#{address}/transactions"
+      @conn.get(url, options)
+    end
+
+    # Provide an array of Bitcoin address.
+    # Returns an array of transactions for a set of Bitcoin address
+    # (array of hashes).
+    def get_addresses_transactions(addresses, options={})
+      get_address_transactions(addresses.join(','), options)
+    end
+
+    # Provide a Bitcoin address.
+    # Returns all op_return data associated with address.
+    def get_address_op_returns(address)
+      url = "/#{API_VERSION}/#{block_chain}/addresses/#{address}/op-returns"
+      @conn.get(url)
+    end
+
+    # Provide a Bitcoin transaction.
+    # Returns basic details for a Bitcoin transaction (hash).
+    def get_transaction(hash)
+      @conn.get("/#{API_VERSION}/#{block_chain}/transactions/#{hash}")
+    end
+
+    # Provide a Bitcoin transaction.
+    # Returns the OP_RETURN string (if it exists) for a Bitcoin
+    # transaction(hash).
+    def get_transaction_op_return(hash)
+      @conn.get("/#{API_VERSION}/#{block_chain}/transactions/#{hash}/op-return")
+    end
+
+    # Provide a hex encoded, signed transaction.
+    # Returns the newly created Bitcoin transaction hash (string).
+    def send_transaction(hex)
+      r = @conn.put("/#{API_VERSION}/#{block_chain}/transactions", {hex: hex})
+      r["transaction_hash"]
+    end
+
+    # Provide a Bitcoin block hash or height.
+    # Returns basic details for a Bitcoin block (hash).
+    def get_block(hash_or_height)
+      @conn.get("/#{API_VERSION}/#{block_chain}/blocks/#{hash_or_height}")
+    end
+
+    # Get latest Bitcoin block.
+    # Returns basic details for latest Bitcoin block (hash).
+    def get_latest_block
+      @conn.get("/#{API_VERSION}/#{block_chain}/blocks/latest")
+    end
+
+    # Provide a Bitcoin block id.
+    # Returns all op_return data contained in a block.
+    def get_block_op_returns(hash_or_height)
+      url = "/#{API_VERSION}/#{block_chain}/blocks/#{hash_or_height}/op-returns"
+      @conn.get(url)
+    end
+
+    def create_webhook(url, id=nil)
+      body = {}
+      body[:url] = url
+      body[:id] = id unless id.nil?
+      @conn.post("/#{API_VERSION}/webhooks", body)
+    end
+    alias_method :create_webhook_url, :create_webhook
+
+    def list_webhooks
+      @conn.get("/#{API_VERSION}/webhooks")
+    end
+    alias_method :list_webhook_url, :list_webhooks
+
+    def update_webhook(id, url)
+      @conn.put("/#{API_VERSION}/webhooks/#{id}", {url: url})
+    end
+    alias_method :update_webhook_url, :update_webhook
+
+    def delete_webhook(id)
+      @conn.delete("/#{API_VERSION}/webhooks/#{id}")
+    end
+    alias_method :delete_webhook_url, :delete_webhook
+
+    def create_webhook_event(id, opts={})
+      body = {}
+      body[:event] = opts[:event] || 'address-transaciton'
+      body[:block_chain] = opts[:block_chain] || block_chain
+      body[:address] = opts[:address] || raise(ChainError,
+        "Must specify address when creating a Webhook Event.")
+      body[:confirmations] = opts[:confirmations] || 1
+      @conn.post("/#{API_VERSION}/webhooks/#{id}/events", body)
+    end
+
+    def list_webhook_events(id)
+      @conn.get("/#{API_VERSION}/webhooks/#{id}/events")
+    end
+
+    def delete_webhook_event(id, event, address)
+      @conn.delete("/#{API_VERSION}/webhooks/#{id}/events/#{event}/#{address}")
+    end
+
+    # Provide a destination address.
+    # Returns a payment address that will automatically forward to the
+    # destination address when funds are sent to it.
+    # If Webhook parameters are provided, a Webhook event will be created
+    # to notify your server when funds are sent to the payment address.
+    def create_payment_address(dest_addr, body={})
+      body[:destination_address] = dest_addr
+      body[:block_chain] ||= block_chain
+      @conn.post("/#{API_VERSION}/payments", body)
+    end
+
+  end
+end
