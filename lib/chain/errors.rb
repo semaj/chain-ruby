afa48b48f05d2203853cd87014b78aae79b3edb5
Oleg Andreev <oleganza@gmail.com>
2014-11-02 16:56:40 +0100

    http and chain error codes to ChainError exception

--- lib/chain/errors.rb
+++ lib/chain/errors.rb
@@ -8,6 +8,16 @@ module Chain
   class ChainFormatError < ChainError; end
 
   # Raised when a network connection fails or server is unreachable.
-  class ChainNetworkError < ChainError; end
+  class ChainNetworkError < ChainError
+    attr_accessor :http_code
+    attr_accessor :chain_code
+    def initialize(message = nil, http_code = nil, chain_code = nil)
+      super(message)
+      @http_code = http_code
+      @chain_code = chain_code
+    end
+  end
 
+  # Raised when transaction cannot be broadcasted because it is invalid or non-canonical.
+  class ChainBroadcastError < ChainError; end
 end
